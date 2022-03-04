cee621f0b1f2db3e642f4ed2cde8902cce5e0f77
amrita-naik <naikamrita254@gmail.com>
2022-03-04 20:29:32 +0530

    Three js

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useRef } from "react";
+import {useFrame } from "@react-three/fiber";
 
 const Box = () => {
-    
+
+  const boxRef = useRef();
+  const matRef = useRef();
+
+  useFrame(({clock}) => {
+    boxRef.current.rotation.z += 0.01;
+    matRef.current.emissiveIntensity = Math.abs(
+      Math.sin(clock.elapsedTime * 0.5)
+    );
+  });
+
   return (
-    <mesh rotation={[30, 0, 10]}>
+    <mesh ref={boxRef} rotation={[30, 0, 10]} scale={2.5}>
         <boxBufferGeometry attach='geometry' args={[2,2,2]} />
         <meshLambertMaterial attach='material' color='#3cd4e9' />    
+        <meshPhysicalMaterial
+          ref={matRef}
+          color={"#3cd4e9"}
+          
+          emissive={"#3cd4e9"}
+          emissiveIntensity={1}
+        />
     </mesh>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
 import './home.css'
+import { BlurPass } from 'postprocessing'
 import Box from '../components/Box'
-
+import { OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
+import React, { useRef, Suspense } from "react";
+import { render } from "react-dom";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import { Canvas } from 'react-three-fiber'
 
 const Home = () => {
+
+  function Effects() {
+    return (
+      <>
+        <EffectComposer>
+          <Bloom
+            intensity={2}
+            luminanceThreshold={0.1}
+            luminanceSmoothing={0.1}
+            blurPass={new BlurPass()}
+          />
+        </EffectComposer>
+      </>
+    );
+  }
+
   return (
     <div className='home'>
         <div className="home-info">
@@ -13,9 +33,13 @@ const Home = () => {
             <button className='home-btn'>Let's Start</button>
         </div>
 
-        <Canvas className='canvas'> 
-            <ambientLight intensity={0.5} />
+        <Canvas className='canvas' style={{ height: 600, width: 600 }}
+          camera={{ position: [0, 0, 10] }}>
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={null}>
             <Box />
+          </Suspense>
+          <Effects />
         </Canvas>
 
     </div>
